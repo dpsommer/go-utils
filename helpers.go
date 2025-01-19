d38de0a62d9168a1f6b42625acb5fe80f56d64e1
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-01-19 07:47:02 -0500

    Add RemoveElement function

--- helpers.go
+++ helpers.go
@@ -1,5 +1,20 @@
 package goutils
 
+func RemoveElement[K comparable](s []K, k K) []K {
+	ret := make([]K, 0, len(s)-1)
+	for i, c := range s {
+		if c == k {
+			ret = append(ret, s[:i]...)
+			ret = append(ret, s[i+1:]...)
+			break
+		}
+	}
+	if len(ret) == 0 {
+		return s
+	}
+	return ret
+}
+
 func Keys[K comparable, T any](m map[K]T) []K {
 	keys := make([]K, len(m))
 
